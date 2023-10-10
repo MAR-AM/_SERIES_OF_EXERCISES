f800223b9a5f18fb5baf98529d5b04cd0407d9f0
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-10-10 21:25:47 +0200

    Add files via upload
    It's a program that returns if the numbers entered have sign or not.

---
+++ 1th exercise.py
@@ -0,0 +1,6 @@
+A=int(input('enter the value of A : '))
+B=int(input('enter the value of B : '))
+if A*B>0:
+    print('A and B are the same sign.')
+elif A*B<0:
+    print('A and B are not the same sign.')
